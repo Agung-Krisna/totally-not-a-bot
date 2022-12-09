2b39fb0bf3124753fad496c60dc493f0ea257e2b
Agung <34364395+Agung-Krisna@users.noreply.github.com>
2022-12-09 23:30:29 +0800

    bug fixes

--- new_diffusion.py
+++ new_diffusion.py
@@ -58,3 +58,9 @@ def inv_shift_rows(s):
     s[2][2], s[3][2], s[0][2], s[1][2] = s[0][2], s[1][2], s[2][2], s[3][2]
     s[3][3], s[0][3], s[1][3], s[2][3] = s[0][3], s[1][3], s[2][3], s[3][3]
 
+
+# learned from http://cs.ucsb.edu/~koc/cs178/projects/JT/aes.c
+xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
+
+
+def mix_single_column(a):
