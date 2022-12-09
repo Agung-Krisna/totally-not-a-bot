5ffa03ea4505c682677211d5e18e26a99247efb2
Agung <34364395+Agung-Krisna@users.noreply.github.com>
2022-12-09 23:40:01 +0800

    testing some things out

--- new_diffusion.py
+++ new_diffusion.py
@@ -76,3 +76,9 @@ xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
 
 
 def mix_single_column(a):
+    # see Sec 4.1.2 in The Design of Rijndael
+    t = a[0] ^ a[1] ^ a[2] ^ a[3]
+    u = a[0]
+    a[0] ^= t ^ xtime(a[0] ^ a[1])
+    a[1] ^= t ^ xtime(a[1] ^ a[2])
+    a[2] ^= t ^ xtime(a[2] ^ a[3])
