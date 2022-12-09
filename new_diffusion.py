3238f7ac9cb1bbd68413861d9007a053894da1a7
Agung <34364395+Agung-Krisna@users.noreply.github.com>
2022-12-09 23:14:15 +0800

    updating requirements

--- new_diffusion.py
+++ new_diffusion.py
@@ -34,3 +34,9 @@ def inv_mix_columns(s):
     # see Sec 4.1.3 in The Design of Rijndael
     for i in range(4):
         u = xtime(xtime(s[i][0] ^ s[i][2]))
+        v = xtime(xtime(s[i][1] ^ s[i][3]))
+        s[i][0] ^= u
+        s[i][1] ^= v
+        s[i][2] ^= u
+        s[i][3] ^= v
+
