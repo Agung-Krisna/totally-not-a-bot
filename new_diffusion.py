5f56f00843cf84bbb555865684dcc9242b7b9fae
Agung <34364395+Agung-Krisna@users.noreply.github.com>
2022-12-10 00:59:13 +0800

    new improvements

--- new_diffusion.py
+++ new_diffusion.py
@@ -154,3 +154,9 @@ def shift_rows(s):
 
 def inv_shift_rows(s):
     s[1][1], s[2][1], s[3][1], s[0][1] = s[0][1], s[1][1], s[2][1], s[3][1]
+    s[2][2], s[3][2], s[0][2], s[1][2] = s[0][2], s[1][2], s[2][2], s[3][2]
+    s[3][3], s[0][3], s[1][3], s[2][3] = s[0][3], s[1][3], s[2][3], s[3][3]
+
+
+# learned from http://cs.ucsb.edu/~koc/cs178/projects/JT/aes.c
+xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
